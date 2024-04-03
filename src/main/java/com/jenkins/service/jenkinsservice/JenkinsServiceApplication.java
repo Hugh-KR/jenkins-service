1af30a9965de8ad438c7fae9fd038dcc618749cf
Hugh <lee2317884@gmail.com>
2024-04-03 18:36:47 +0900

    project init

---
+++ src/main/java/com/jenkins/service/jenkinsservice/JenkinsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.service.jenkinsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsServiceApplication.class, args);
+	}
+
+}
